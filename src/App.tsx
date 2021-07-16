4beb20a8a4f97f09c0af5c1fb429d84f670f23d4
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-16 17:41:58 +0200

    Add props and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import "./App.css";
 import { VideoGallery } from "./components/VideoGallery/VideoGallery";
 
 const App = () => {
     return (
-        <div className="">
-            <VideoGallery>
+        <div className="app">
+            <VideoGallery tileAspectRatio={4 / 3} tilePadding={"1rem"}>
                 <div>child</div>
                 <div>child</div>
                 <div>child</div>
--- src/components/VideoGallery/VideoGallery.tsx
+++ src/components/VideoGallery/VideoGallery.tsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import "./VideoGallery.css";
+
 interface Props {
     children: React.ReactNode;
+    tileAspectRatio: Number;
+    tilePadding: String;
 }
 
 export const VideoGallery: React.FC<Props> = ({ children }) => {
-    return <div>{children}</div>;
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+
+    return <div className="video-gallery">{children}</div>;
 };
