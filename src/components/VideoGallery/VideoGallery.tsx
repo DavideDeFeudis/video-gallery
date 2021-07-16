63b5e0db65017372f9a58b8a64d8672e8410eebf
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-16 17:59:43 +0200

    Add ResizeObserver

--- src/components/VideoGallery/VideoGallery.tsx
+++ src/components/VideoGallery/VideoGallery.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./VideoGallery.css";
 
 interface Props {
@@ -10,6 +10,25 @@ interface Props {
 export const VideoGallery: React.FC<Props> = ({ children }) => {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
+    const galleryRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
-    return <div className="video-gallery">{children}</div>;
+    useEffect(() => {
+        const observer = createResizeObserver();
+        observer.observe(galleryRef.current);
+        return () => observer.disconnect();
+    }, []);
+
+    const createResizeObserver = () => {
+        return new ResizeObserver((entries) => {
+            const { width, height } = entries[0].contentRect;
+            setWidth(width);
+            setHeight(height);
+        });
+    };
+
+    return (
+        <div className="video-gallery" ref={galleryRef}>
+            {children}
+        </div>
+    );
 };
