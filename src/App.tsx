32b47dddcd7a9c0e651ae82e0ef4fb8abc42df93
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-17 14:21:30 +0200

    Add basic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { VideoGallery } from "./components/VideoGallery/VideoGallery";
 
 const App = () => {
+    const [children, setChildren] = useState<number[]>([]);
+
+    useEffect(() => {
+        for (let i = 0; i < 3; i++) {
+            setChildren((prev) => [...prev, i + 1]);
+        }
+    }, []);
+
     return (
         <div className="app">
             <VideoGallery tileAspectRatio={4 / 3} tilePadding={"1rem"}>
-                <div>child</div>
-                <div>child</div>
-                <div>child</div>
-                <div>child</div>
+                {children.map((item) => (
+                    <div key={item}>child {item}</div>
+                ))}
             </VideoGallery>
         </div>
     );
--- src/components/VideoGallery/VideoGallery.tsx
+++ src/components/VideoGallery/VideoGallery.tsx
@@ -1,22 +1,77 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
 import "./VideoGallery.css";
 
+const calculateLayout = (containerWidth: number, containerHeight: number, tileCount: number, aspectRatio: number) => {
+    let bestLayout = {
+        area: 0,
+        cols: 0,
+        rows: 0,
+        width: 0,
+        height: 0,
+    };
+
+    for (let cols = 1; cols <= tileCount; cols++) {
+        const rows = Math.ceil(tileCount / cols);
+        const hScale = containerWidth / (cols * aspectRatio);
+        const vScale = containerHeight / rows;
+        let width;
+        let height;
+        if (hScale <= vScale) {
+            width = Math.floor(containerWidth / cols);
+            height = Math.floor(width / aspectRatio);
+        } else {
+            height = Math.floor(containerHeight / rows);
+            width = Math.floor(height * aspectRatio);
+        }
+        const area = width * height;
+        if (area > bestLayout.area) {
+            bestLayout = {
+                area,
+                width,
+                height,
+                rows,
+                cols,
+            };
+        }
+    }
+    return bestLayout;
+};
+
 interface Props {
-    children: React.ReactNode;
-    tileAspectRatio: Number;
-    tilePadding: String;
+    children: any;
+    tileAspectRatio: number;
+    tilePadding: string;
 }
 
-export const VideoGallery: React.FC<Props> = ({ children }) => {
+export const VideoGallery: React.FC<Props> = ({ children, tileAspectRatio, tilePadding }) => {
     const [width, setWidth] = useState(0);
     const [height, setHeight] = useState(0);
+    const [childStyle, setChildStyle] = useState({});
     const galleryRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+    const wrapperRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+    const childrenCount = React.Children.count(children);
 
     useEffect(() => {
         const observer = createResizeObserver();
-        observer.observe(galleryRef.current);
+        observer.observe(wrapperRef.current);
         return () => observer.disconnect();
     }, []);
+
+    useLayoutEffect(() => {
+        const { width: tileWidth, height: tileHeight, cols } = calculateLayout(width, height, childrenCount, tileAspectRatio);
+        console.log("childrenCount:", childrenCount);
+        console.log("tileWidth:", tileWidth);
+        console.log("tileHeight:", tileHeight);
+        console.log("cols:", cols);
+        galleryRef.current.style.setProperty("--width", tileWidth + "px");
+        galleryRef.current.style.setProperty("--cols", cols + "");
+
+        setChildStyle({
+            width: `${tileWidth}px`,
+            height: `${tileHeight}px`,
+            padding: `${tilePadding}`,
+        });
+    }, [width, height, childrenCount, tileAspectRatio, children, tilePadding]);
 
     const createResizeObserver = () => {
         return new ResizeObserver((entries) => {
@@ -27,8 +82,31 @@ export const VideoGallery: React.FC<Props> = ({ children }) => {
     };
 
     return (
-        <div className="video-gallery" ref={galleryRef}>
-            {children}
+        <div className="wrapper" ref={wrapperRef}>
+            <div className="video-gallery" ref={galleryRef}>
+                {children.map((child: React.ReactChild, i: number) => (
+                    <div className="child" key={i} style={childStyle}>
+                        {child}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
+
+// React.Children.map(children, (child) => {
+//     return React.cloneElement(child, { style: { background: "blue" } });
+// });
+
+// for (let i = 0; i < children.length; i++) {
+//     const tile = children[i];
+//     tile.style.width = `${tileWidth}px`;
+//     tile.style.height = `${tileHeight}px`;
+//     tile.style.padding = `${tilePadding}`;
+// }
+
+// children?.forEach((tile) => {
+//     tile.style.width = `${tileWidth}px`;
+//     tile.style.height = `${tileHeight}px`;
+//     tile.style.padding = `${tilePadding}`;
+// });
