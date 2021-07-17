4791964b5976f37863625fc5a6e36263693512d1
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-17 15:13:13 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import { VideoGallery } from "./components/VideoGallery/VideoGallery";
 
 const App = () => {
-    const [children, setChildren] = useState<number[]>([0, 1, 2]);
-
-    // useEffect(() => {
-    //     for (let i = 0; i < 3; i++) {
-    //         setChildren((prev) => [...prev, i + 1]);
-    //     }
-    // }, []);
-
     return (
         <div className="app">
             <VideoGallery tileAspectRatio={4 / 3} tilePadding={"1rem"}>
-                {children.map((item) => (
+                {[0, 1, 2].map((item) => (
                     <video className="child" key={item}></video>
                 ))}
             </VideoGallery>
